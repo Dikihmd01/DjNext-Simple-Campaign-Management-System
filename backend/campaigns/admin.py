6f60185d15b97f3f96474ef996e3396dd884fb9f
Dikihmd01 <dikihamdani.id@gmail.com>
2024-06-11 21:23:22 +0700

    Customize admin site

--- backend/campaigns/admin.py
+++ backend/campaigns/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Campaign, Subscriber
 
-admin.site.register(Campaign)
-admin.site.register(Subscriber)
+class CampaignAdmin(admin.ModelAdmin):
+    list_display = ("title", "create_date", "update_date")
+    search_fields = ("title", "description")
+    list_per_page = 10
+
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ("email", "campaign", "create_date", "update_date")
+    search_fields = ("email", "campaign__title")
+    list_per_page = 10
+
+admin.site.register(Campaign, CampaignAdmin)
+admin.site.register(Subscriber, SubscriberAdmin)
