0f35d998785746e41d13cb6e3b5b7f82bd24fca0
Dikihmd01 <dikihamdani.id@gmail.com>
2024-06-13 21:08:52 +0700

    Improve log warning

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:ital,wght@0,300..800;1,300..800&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet" />
       </Head>
       <body>
